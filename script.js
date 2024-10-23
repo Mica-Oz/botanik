df616e4476575a9e18dfcb79a82754e5e6a8844e
mica-oz <kahulaoz@gmail.com>
2024-10-22 23:26:11 -0700

    prac-sub filling in

--- script.js
+++ script.js
@@ -3,15 +3,10 @@ $(document).ready(function () {
     let prac = $(this);
     prac.toggleClass("expanded");
     if (prac.hasClass("expanded")) {
-      prac.siblings().hide();
-      prac.animate({ width: "79vw" });
-
-      prac.find(".prac-sub").slideToggle();
-    } else {
-      prac.find(".prac-sub").slideToggle();
-      prac.animate({ width: "26vw" }, 400, function () {
-        prac.siblings().show();
-      });
     }
+    // prac.children(".offerings").fadeOut();
+
+    // prac.siblings().children(".offerings").fadeOut();
+    prac.find(".prac-sub").slideToggle();
   });
 });
